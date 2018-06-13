6c7131c231c88d2f965bfa5de64cc2e313d02d3f
Andrey Smirnov <andrew_1024@mail.ru>
2018-06-13 22:51:14 +0300

    add popap

--- src/assets/scripts/about.js
+++ src/assets/scripts/about.js
@@ -1,8 +1,10 @@
 import "./modules/example";
 
 var menuBtn = document.querySelector('.hero__menu');
+var popap = document.querySelector('.popap-page');
 
 menuBtn.addEventListener('click', function (e) {
     e.preventDefault();
     menuBtn.classList.toggle("active");
+    popap.classList.toggle("is-active");
 })
