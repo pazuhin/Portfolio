7faab5fbd71955698ca36b3b67631020e2d80b8e
Andrey Smirnov <andrew_1024@mail.ru>
2018-06-13 21:19:17 +0300

    transform menu humb

--- src/assets/scripts/about.js
+++ src/assets/scripts/about.js
@@ -1,1 +1,8 @@
 import "./modules/example";
+
+var menuBtn = document.querySelector('.hero__menu');
+
+menuBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    menuBtn.classList.toggle("active");
+})
