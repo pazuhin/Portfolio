db68460ea39b730b0e97999d785be2cd128bc9de
Andrey Smirnov <andrew_1024@mail.ru>
2018-06-16 19:34:33 +0300

    add flip login, fix bugs

---
+++ src/assets/scripts/works.js
@@ -0,0 +1,8 @@
+var menuBtn = document.querySelector('.hero__menu');
+var popap = document.querySelector('.popap-page');
+
+menuBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    menuBtn.classList.toggle("active");
+    popap.classList.toggle("is-active");
+})
