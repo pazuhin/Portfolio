504bd20053d24d2de4285f5e3fed1e7672a370b2
Andrey Smirnov <andrew_1024@mail.ru>
2018-06-23 14:22:13 +0300

    work on adaptiv

--- src/assets/scripts/about.js
+++ src/assets/scripts/about.js
@@ -8,3 +8,15 @@ menuBtn.addEventListener('click', function (e) {
     menuBtn.classList.toggle("active");
     popap.classList.toggle("is-active");
 })
+
+//map
+
+function initMap() {
+    var centerLatLng = new google.maps.LatLng(56.2928515, 43.7866641);
+    var mapOptions = {
+        center: centerLatLng,
+        zoom: 8
+    };
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+
